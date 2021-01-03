7204d58eb28bf77ee9d24c7234179fc09cce9a8a
huaifufeng <huaifufeng@163.com>
2021-01-03 21:18:44 +0800

    完成两数相加

---
+++ common.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class ListNode
+ * 链表的元素定义
+ */
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
---
+++ problems/2.add-two-numbers.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @param ListNode $l1
+ * @param ListNode $l2
+ * @return ListNode
+ */
+function addTwoNumbers($l1, $l2)
+{
+    $head = $l = new ListNode();
+    $v = 0;
+
+    while ($l1 != null || $l2 != null) {
+        $v1 = $v2 = 0;
+        if ($l1 != null) {
+            $v1 = $l1->val;
+            $l1 = $l1->next;
+        }
+
+        if ($l2 != null) {
+            $v2 = $l2->val;
+            $l2 = $l2->next;
+        }
+
+        $temp = $v + $v1 + $v2;
+
+        if ($temp >= 10) {
+            $v = 1;
+            $temp = $temp - 10;
+        } else {
+            $v = 0;
+        }
+
+        $l->next = new ListNode($temp, null);
+        $l = $l->next;
+    }
+
+    if ($v > 0) {
+        $l->next = new ListNode($v, null);
+    }
+
+
+    return $head->next;
+}
