a360c46de205089381016bfa2be15c46e5910dbe
huaifufeng <huaifufeng@163.com>
2021-01-03 21:02:22 +0800

    完成两数之和

---
+++ problems/1.two-sum.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * https://leetcode-cn.com/problems/two-sum/submissions/
+ *
+ * 1、两数之和
+ *
+ */
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target)
+    {
+        $checkNums = [];
+        $length = count($nums);
+        for ($i = 0; $i < $length; $i++) {
+            $left = $target - $nums[$i];
+            $lastIndex = $checkNums[$left] ?? -1;
+            if ($lastIndex == -1) {
+                $checkNums[$nums[$i]] = $i;
+            } else {
+                return [$lastIndex, $i];
+            }
+        }
+    }
+}
